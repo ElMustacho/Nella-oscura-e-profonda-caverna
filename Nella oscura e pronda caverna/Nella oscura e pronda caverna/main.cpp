136c249a01605d2438255d2c258c6a20c285eaef
Fabrizio <Fabrizio@FABRIZIO-PC>
2017-06-11 18:57:08 +0200

    Tolto distruttore inutile

--- Nella oscura e pronda caverna/Nella oscura e pronda caverna/main.cpp
+++ Nella oscura e pronda caverna/Nella oscura e pronda caverna/main.cpp
@@ -5,7 +5,6 @@ int main() {
 	Oggetto idolo(15., "Hatkanda", "L'oggetto di un film generico", 400); //CHECK
 	std::cout << idolo.getDescrizione() << std::endl << idolo.getNome() << std::endl << idolo.getPeso() << std::endl <<
 		idolo.getValore();
-	//idolo.~Oggetto();
 	char a;
 	std::cout << std::endl;
 	std::cin >> a;
