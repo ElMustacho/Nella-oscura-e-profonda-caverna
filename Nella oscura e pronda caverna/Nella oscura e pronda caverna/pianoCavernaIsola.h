d313ebd2ccdaee0597c2b6f05b1345e827b35935
Fabrizio <Fabrizio@FABRIZIO-PC>
2017-08-05 16:38:42 +0200

    Aggiunta libreria perlin noise, aggiunto piano generato automaticamente (solo il pavimento).

---
+++ Nella oscura e pronda caverna/Nella oscura e pronda caverna/pianoCavernaIsola.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Piano.h"
+class pianoCavernaIsola :
+	public Piano
+{
+public:
+	pianoCavernaIsola(int a, int b); //Prende la dimensione del piano, restituisce la posizione del personaggio
+	~pianoCavernaIsola();
+};
+
