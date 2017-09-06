63fd896c3bd2e96662f1dcc864f307a52cf2980a
Fabrizio <Fabrizio@FABRIZIO-PC>
2017-09-06 19:11:19 +0200

    Installato SFML, nel main è presente un test.

--- Nella oscura e pronda caverna/Nella oscura e pronda caverna/main.cpp
+++ Nella oscura e pronda caverna/Nella oscura e pronda caverna/main.cpp
@@ -8,8 +8,29 @@
 #include "PianoFile.h"
 #include "Danno.h"
 #include "pianoCavernaIsola.h"
+#include "SFML\Graphics.hpp"
 int main()
 {
+	// SFML TEST!!!!!!
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	//
 	std::list<std::shared_ptr<Oggetto>> tabellaLoot; //VUOTA
 	std::vector<std::shared_ptr<Oggetto>> Equipaggiamento;
 	std::vector<Entita> tabellaEntita; //VUOTA
