6b808717ab6551bceb4cb93417d999707b8d94a9
fabio <fabiopolonia00@gmail.com>
2017-09-21 17:23:59 +0200

    Summary  - Fixed after merge

--- Nella oscura e pronda caverna/Nella oscura e pronda caverna/pianoCavernaIsolaGrafica.h
+++ Nella oscura e pronda caverna/Nella oscura e pronda caverna/pianoCavernaIsolaGrafica.h
@@ -9,12 +9,12 @@ class pianoCavernaIsolaGrafica :
 public:
 
 	int playPiano(char bloat='0');
-	int playerAct(bool loquace, sf::Window &window);
+	//int playerAct(bool loquace, sf::Window &window);
 	pianoCavernaIsolaGrafica(int larghezza, int lunghezza, std::string posizioneFile = "", std::vector<std::shared_ptr<Oggetto>> oggettiPossibili = {}, std::shared_ptr<Entita> player=nullptr, std::vector<std::shared_ptr<Entita>> entitaGenerabili = {});
 
-	int playPiano();
+	//int playPiano();
 	int playerAct(bool loquace, sf::RenderWindow &window, std::vector<Casella> pavimento, int larghezza, int lunghezza, sf::Sprite tiles, sf::Sprite ogg, sf::Sprite prot, sf::Sprite enem, TextBox& messages);
-	pianoCavernaIsolaGrafica(int larghezza, int lunghezza);
+	//pianoCavernaIsolaGrafica(int larghezza, int lunghezza);
 
 	void stampaPianoSuFinestra();
 	~pianoCavernaIsolaGrafica();
